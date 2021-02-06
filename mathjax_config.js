83a6ba5304d66f2790c63b9f61100d2a7ec6c26e
Fred Feng <fredfeng@umich.edu>
2021-02-06 17:31:05 -0500

    Just in case

--- mathjax_config.js
+++
@@ -1,8 +0,0 @@
-MathJax.Hub.Config({
-    tex2jax: {
-    skipTags: ['script', 'noscript', 'style', 'textarea', 'pre'], 
-    inlineMath: [['$','$'], ['$','$']]
-    },
-    "HTML-CSS": { scale: 100 }   // change default font size
-});
-MathJax.Hub.Configured();
